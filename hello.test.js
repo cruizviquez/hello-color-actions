d53c2749f839961fc06baae03e489adaeda9ac0b
cruizviquez <cruizviquez@gmail.com>
2023-01-20 19:13:37 -0600

    CiudaddeMexico

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Ciudad de Mexico");
+    expect(hello.hello()).toEqual("Hola Ciudad de México");
   });
 });
